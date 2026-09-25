c04e5837ff87e2994e7e7464ec91f03dd67bcac5
Nitish Kumar <nitishcoder8@gmail.com>
2026-09-25 22:49:34 +0530

    Add files via upload

---
+++ my pydroid 02.py
@@ -0,0 +1,10 @@
+numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for no in numbers :
+    if no == 1 :
+        print("1st")
+    elif no == 2 :
+       print("\n2nd")
+    elif no == 3 :
+       print("\n3rd")
+    else :
+       print(f"\n{no}th")
